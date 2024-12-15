3b949cdae689a2bcadde1ff2e52caec424943962
Vladyslav <kramarenko.creator@gmail.com>
2024-12-15 23:42:06 +0200

    Redirected url favicon.ico

--- itcoursemarket/urls.py
+++ itcoursemarket/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicons/favicon.ico', permanent=True)),
 ]
