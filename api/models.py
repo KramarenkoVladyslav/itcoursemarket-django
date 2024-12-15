168868d0974bbc90523fbbb205f1185af08b4a2f
Vladyslav <kramarenko.creator@gmail.com>
2024-12-15 16:39:13 +0200

    Implemented API

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
 from shop.models import Category, Course
 
+
 class CategoryResource(ModelResource):
     class Meta:
         queryset = Category.objects.all()
@@ -12,4 +13,5 @@ class CourseResource(ModelResource):
         queryset = Course.objects.all()
         resource_name = 'courses'
         allowed_methods = ['get', 'post', 'delete']
+
 
--- itcoursemarket/urls.py
+++ itcoursemarket/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from tastypie.api import Api
+
+from api.models import CategoryResource, CourseResource
+
+api = Api(api_name='v1')
+course_resource = CourseResource()
+category_resource = CategoryResource()
+api.register(course_resource)
+api.register(category_resource)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('api/', include(api.urls))
 ]
